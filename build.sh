99d9ee1aa5fbee0dbc5bdd10a664f77e0426552d
Erik Gerk <e.gerk@fn.de>
2026-03-26 21:03:13 +0100

    build.sh für Subtree angepasst

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-g++ -std=c++20 -Iinclude -Iinclude/engine -Iinclude/ui src/*/*.cpp -o tictactoe
+g++ -std=c++20 -Iinclude -Iinclude/engine -Iinclude/ui src/*/*.cpp -Iexternal/jsoncpp/include -Iexternal/rang/include external/jsoncpp/src/lib_json/* -o tictactoe
